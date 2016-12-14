9a2465076e5e7c18fef5971d8d3b499b7534c88f
Zim <cyborg.synth.a1f@gmail.com>
2016-12-14 22:38:21 +0200

    Была произведена оптимизация кода

--- get_list.py
+++ get_list.py
@@ -7,7 +7,7 @@ import xml.etree.ElementTree as ET
 import sqlite3
 
 def lerning_XML():
-    tag = input('enter tag:')
+    tag = 34 # input('enter tag:')
     url = 'https://api.eve-central.com/api/quicklook?typeid='+str(tag)
     response = urllib.request.urlopen(url)
     data = response.read()
@@ -25,39 +25,33 @@ def data_sqlite():#создание таблици
     c.execute("CREATE TABLE IF NOT EXISTS Region(Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, Name STRING)")
     c.execute("CREATE TABLE IF NOT EXISTS Security(Id INTEGER, Status FLOAT)")
     c.execute("CREATE TABLE IF NOT EXISTS Station(Id INTEGER PRIMARY KEY NOT NULL, Region_ID INTEGER REFERENCES Region (Id), Security_ID INTEGER REFERENCES Security (Id), Name STRING)")
+    conn.commit()
     c.close()
     conn.close()
 
-def filling_DB(): #Заполнение таблици данными
+def filling_DB2(): #тест ситаксиса
     data_sqlite()
     a = lerning_XML()
     buy = a.findall('quicklook/buy_orders/order')
     sall = a.findall('quicklook/sell_orders/order')
 
-    cread_tablet = ['Buy_order', 'Sall_order']
-
     conn = sqlite3.connect('Eve_online.db')
-    c=conn.cursor()
-
-    for ord in buy:
-        price = ord.findtext('price')
-        station_name = ord.findtext('station_name')
-        security = ord.findtext('security')
-        region = ord.findtext('region')
-        tablet = [int(region), float(security), str(station_name) ,float(price)]
-        c.execute("INSERT INTO Buy_order(Region, Security, Station, Price) VALUES (?, ?, ?, ?)", tablet)
-    for ordd in sall:
-        price = ordd.findtext('price')
-        station_name = ordd.findtext('station_name')
-        security = ordd.findtext('security')
-        region = ordd.findtext('region')
-        tablet = [int(region), float(security), str(station_name) ,float(price)]
-        c.execute("INSERT INTO Sall_order(Region, Security, Station, Price) VALUES (?, ?, ?, ?)",tablet)
+    c = conn.cursor()
+    orders = {"Buy_order": buy, "Sall_order": sall}
+    for key in orders:
+        for item in orders[key]:
+            price = item.findtext('price')
+            station_name = item.findtext('station_name')
+            security = item.findtext('security')
+            region = item.findtext('region')
+            tablet = [int(region), float(security), str(station_name) ,float(price)]
+            c.execute("INSERT INTO " + key + "(Region, Security, Station, Price) VALUES (?, ?, ?, ?)", tablet)
+    conn.commit()
     c.close()
     conn.close()
 
 def main():
-    filling_DB()
+    filling_DB2()
 
 if __name__ == '__main__':
     main()
